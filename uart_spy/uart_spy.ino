4fd0441d338c9ba79a630d1a279578f53ef1986d
gilbertwaltoon <gilbertwaltoon@gmail.com>
2024-10-27 11:27:19 +0000

    Improved uart_spy

--- uart_spy/uart_spy.ino
+++ uart_spy/uart_spy.ino
@@ -6,20 +6,32 @@
 #include <Wire.h>
 
 #define UART_SPEED 9600
-#define rxPin 10
-#define txPin 11
+/* On the Uno black-switch optionally selects 3V3 */
+#define rxPin 10  /* On Uno 10 is marked 10; On Nano 11 is marked D11 */ 
+#define txPin 11  /* On Uno 11 is marked 11; On Nano 11 is marked D11 */
+
 
 SoftwareSerial uart_spy(rxPin, txPin);
 
 void setup() {
-  /* Setup screen terminal */
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+ 
+  /* Setup putty-type serial monitor terminal */
   Serial.begin(9600);
   Serial.println("Starting...");
   /* Setup uart spy */
   uart_spy.begin(9600);
-  Serial.println("Spying traffic...");
+  Serial.println("Spying traffic....");
 }
 
 void loop() {
   if (uart_spy.available()) Serial.write(uart_spy.read());
+
+  /* UART Write example  
+  int bytessent = uart_spy.write("hello\n\r");
+  Serial.println(bytessent);
+  delay(500);
+  */
+
 }
